7ac02534d64ccf03cd494660eb94fd8bb7de0d2a
Noel Cruz <nocruz@activision.com>
2022-07-25 15:50:53 -0400

    created Car class to reference as an example

---
+++ Vehicle-Classes/Car.cpp
@@ -0,0 +1,25 @@
+#include "Car.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Car
+{
+	// Member variables
+	int m_year;
+	int m_miles;
+	string m_brand;
+
+	// Member functions
+	void Display()
+	{
+		cout << "Brand: " << m_brand << endl;
+		cout << "Year: " << m_year << endl;
+		cout << "Miles: " << m_miles << endl;
+	}
+
+	void Drive(int miles)
+	{
+		m_miles = m_miles + miles;
+	}
+};
