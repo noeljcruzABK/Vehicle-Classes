feb7dbb676bb88e69d6513fb47834d491a9b23fd
Noel Cruz <nocruz@activision.com>
2022-07-25 15:37:52 -0400

    cleared comments

--- Vehicle-Classes/Vehicle-Classes.cpp
+++ Vehicle-Classes/Vehicle-Classes.cpp
@@ -1,20 +1,12 @@
-// Vehicle-Classes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Create a vehicle base class, and from that create the necessary classes to get to boat, car and airplane.
+// All classes should have an overridable function named Drive.
+// Create an object of each and demonstrate the Drive function works.
+// Feel free to add any additional functionality to better simulate these vehicles!
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
